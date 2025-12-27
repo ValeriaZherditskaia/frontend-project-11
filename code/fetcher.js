6ace8af7e829a0a2025780abe1093b93ffe3c53d
Valeria Zherditskaia <st.leron@mail.ru>
2025-12-27 14:18:11 +0300

    fix: добавить обработку ошибок сети в fetchRss

--- code/fetcher.js
+++ code/fetcher.js
@@ -6,14 +6,28 @@ const fetchRss = async (url) => {
 
   // Указываем параметры запроса
   const params = {
-    url,
-    disableCache: true, // Отключение кеша
+    url, // URL RSS фида который нужно загрузить
+    disableCache: true, // Отключение кеша для получения свежих данных
   };
 
-  // Делаем GET-запрос через axios
-  const response = await axios.get(proxyUrl, { params });
+  try {
+    // Пытаемся выполнить GET-запрос через axios
+    const response = await axios.get(proxyUrl, { params });
 
-  // Возвращаем содержимое (XML-строку)
-  return response.data.contents;
+    // Проверяем есть ли ошибка в ответе прокси
+    if (!response.data.contents) {
+      // Если прокси вернул пусто или ошибку (contents = null)
+      const error = new Error('fetch_error');
+      throw error;
+    }
+
+    // Возвращаем содержимое (XML-строку)
+    return response.data.contents;
+  } catch (error) {
+    // Если произойдёт ошибка сети, timeout или другая ошибка axios
+    const err = new Error('fetch_error');
+    throw err;
+  }
 };
+
 export default fetchRss;
