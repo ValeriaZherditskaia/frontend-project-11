4a0d58ad6e35504f727898f4c6f8d427f5a436c4
Valeria Zherditskaia <st.leron@mail.ru>
2026-01-06 15:52:11 +0300

    fix: feedback display и form unlock

--- code/main.js
+++ code/main.js
@@ -1,18 +1,6 @@
 import './style.css'
 import 'bootstrap'
-import i18next from './i18n.js'
+import './i18n.js'
 import application from './application.js'
 
-// Ждем инициализации i18next перед запуском
-i18next.init({
-  lng: 'ru',
-  debug: false,
-  resources: {
-    ru: { translation: require('./locales/ru.json') },
-    en: { translation: require('./locales/en.json') },
-  },
-}).then(() => {
-  application()
-}).catch(err => {
-  console.error('i18n init failed:', err)
-})
+application()
